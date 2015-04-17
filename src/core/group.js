63820116682fac82ad2b2e16077ae71a48160e53
Ciro Cesar Maciel <ciro.maciel@c37.co>
2015-04-17 16:32:35 -0300

    alguns testes antes de iniciar a versão 4

--- src/core/group.js
+++ src/core/group.js
@@ -1,9 +1,12 @@
 define("plane/core/group", ['require', 'exports'], function (require, exports) {
 
+    var intersection = require('plane/math/intersection');
+
+    var point = require('plane/core/point'),
+        view = require('plane/core/view');
+
     var utility = require('utility');
 
-    var point = require('plane/core/point');
-    
     var store = utility.data.dictionary.create();
 
 
@@ -20,12 +23,63 @@ define("plane/core/group", ['require', 'exports'], function (require, exports) {
 
     Group.prototype.contains = function (position, transform) {
 
-        return true;
+        var scale = Math.sqrt(transform.a * transform.d);
+        var move = point.create(transform.tx, transform.ty);
+
+        this.children.forEach(function (shape) {
+
+            var segmentA = null,
+                segmentB = null;
+
+            for (var i = 0; i < shape.segments.length; i++) {
+
+                // correção de lógica, como estou calculando pelos segmentos, 
+                // não posso pegar o ultimo + o primeiro, pois será como um shape 'fechado'
+                // 2015.02.16 - outra correção IMPORTANTE - se == ao fim CONTINUE
+                if (i + 1 === this.segments.length) {
+                    continue;
+                }
+
+                segmentA = this.segments[i];
+                segmentB = this.segments[i + 1];
+
+                if (intersection.circleLine(position, 4, point.create(segmentA.x * scale + move.x, segmentA.y * scale + move.y), point.create(segmentB.x * scale + move.x, segmentB.y * scale + move.y)))
+                    return true;
+            }
+
+        })
+
+        return false;
     };
 
     Group.prototype.intersect = function (rectangle) {
 
-        return true;
+        var tl = point.create(rectangle.from.x, rectangle.to.y), // top left
+            tr = point.create(rectangle.to.x, rectangle.to.y), // top right
+            bl = point.create(rectangle.from.x, rectangle.from.y), // bottom left
+            br = point.create(rectangle.to.x, rectangle.from.y), // bottom right
+            result = false;
+
+
+        this.children.forEach(function (shape) {
+
+            if ((shape.type === 'line') || (shape.type === 'text')) {
+
+                // faço a verificação 'pontos dentro do retângulo'
+                result = ((shape.to.x >= rectangle.from.x && shape.to.x <= rectangle.to.x) && (shape.to.y >= rectangle.from.y && shape.to.y <= rectangle.to.y)) ||
+                    ((shape.from.x >= rectangle.from.x && shape.from.x <= rectangle.to.x) && (shape.from.y >= rectangle.from.y && shape.from.y <= rectangle.to.y));
+
+                if (!result) {
+                    result = intersection.segmentsRectangle(shape.segments, tl, tr, bl, br);
+                }
+
+            } else {
+                result = intersection.segmentsRectangle(shape.segments, tl, tr, bl, br);
+            }
+
+        });
+
+        return result;
     };
 
     Group.prototype.calculeBounds = function () {
@@ -36,13 +90,43 @@ define("plane/core/group", ['require', 'exports'], function (require, exports) {
             childrenSegmentsPoints = childrenSegmentsPoints.concat(shape.segments);
         });
 
-        var maxPoint = childrenSegmentsPoints.filter(function(pointCheck, idx, arr){
-            return 0
-            //return pointCheck.maximum()
-        }),
-            minPoint = 0;
+        var maxPoint = childrenSegmentsPoints[0],
+            minPoint = childrenSegmentsPoints[0];
 
-        console.log(childrenSegmentsPoints);
+        childrenSegmentsPoints.forEach(function (pointCheck) {
+
+            pointCheck = point.create(pointCheck);
+
+            maxPoint = pointCheck.maximum(maxPoint);
+            minPoint = pointCheck.minimum(minPoint);
+
+        });
+
+        // um remendo para o calculo
+        var angleInRadian = maxPoint.angleTo(minPoint),
+            lineSizeValue = 5 / view.zoom;
+
+        // com uma tolerancia para os limites não ficar sem cima dos shapes
+        var maxPoint2 = point.create(maxPoint.x + (-lineSizeValue * Math.cos(angleInRadian)), maxPoint.y + (-lineSizeValue * Math.sin(angleInRadian))),
+            minPoint2 = point.create(minPoint.x + (+lineSizeValue * Math.cos(angleInRadian)), minPoint.y + (+lineSizeValue * Math.sin(angleInRadian)));
+
+
+//        var maxPoint2 = maxPoint,
+//            minPoint2 = minPoint;
+
+
+        //console.log(maxPoint);
+        //console.log(maxPoint2);
+
+        //console.log(minPoint);
+        //console.log(minPoint2);
+
+        this.bounds = {
+            bottomLeft: minPoint2,
+            topLeft: point.create(minPoint2.x, maxPoint2.y),
+            topRight: maxPoint2,
+            bottomRight: point.create(maxPoint2.x, minPoint2.y)
+        };
 
         return true;
     };
