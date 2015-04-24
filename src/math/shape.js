045fbaa893bcca61f65ea9c53266553c4ea44697
Ciro Cesar Maciel <me@ciro-maciel.me>
2015-04-24 11:50:16 -0300

    mais uma tentativa de cache

--- src/math/shape.js
+++ src/math/shape.js
@@ -15,11 +15,12 @@
             this.name = plane.utility.string.format('{0} - {1}', [attrs.type, attrs.uuid]);
 
             // cache
-            this._cache = {
-                //canvas: document.createElement("canvas"),
-                canvas: null,
-                context: null
-            };
+//            this._cache = {
+//                canvas: document.createElement("canvas"),
+//                //canvas: null,
+//                context: null,
+//                image: null
+//            };
 
 
             // completando os campos do shape
@@ -34,6 +35,7 @@
             // calculando os limites
             this._calculeBounds();
 
+            // montando o cache
             //this._calculeCache();
 
             return true;
@@ -56,11 +58,22 @@
         _calculeCache: function () {
             // http://www.createjs.com/Demos/EaselJS/Cache
             // https://github.com/CreateJS/EaselJS/blob/master/src/easeljs/display/DisplayObject.js#L811
-            
+
             // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Optimizing_canvas
             // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial
 
-            this._cache.context = this._cache.canvas.getContext('2d');
+            if (this._cache.context === null) {
+
+                this._cache.canvas.width = plane.view.size.width;
+                this._cache.canvas.height = plane.view.size.height;
+
+                this._cache.context = this._cache.canvas.getContext('2d');
+
+            }
+
+            // clear context, +1 is needed on some browsers to really clear the borders
+            this._cache.context.clearRect(0, 0, this._cache.canvas.width + 1, this._cache.canvas.height + 1);
+
 
             var moveX = ~~(0.5 + (this._segments[0].x)),
                 moveY = ~~(0.5 + (this._segments[0].y));
@@ -68,16 +81,24 @@
             // movendo para o inicio do shape para não criar uma linha
             this._cache.context.moveTo(moveX, moveY);
 
+            this._cache.context.beginPath();
             // para cada segmento, vou traçando uma linha
             for (var i = 0; i < this._segments.length; i++) {
-                // http://seb.ly/2011/02/html5-canvas-sprite-optimisation/
-                // http://jsperf.com/math-round-vs-hack/3
-                // http://www.ibm.com/developerworks/library/wa-canvashtml5layering/
+
                 var x = ~~(0.5 + (this._segments[i].x));
                 var y = ~~(0.5 + (this._segments[i].y));
 
                 this._cache.context.lineTo(x, y);
             }
+            this._cache.context.stroke();
+
+            // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/getImageData
+            var x = plane.utility.math.parseFloat(this._bounds.from.x, 5),
+                y = plane.utility.math.parseFloat(this._bounds.from.y, 5),
+                width = this._bounds.width === 0 ? 1 : this._bounds.width,
+                height = this._bounds.height === 0 ? 1 : this._bounds.height;
+
+            this._cache.image = this._cache.context.getImageData(x, y, width, height);
 
             return true;
         },
@@ -114,85 +135,30 @@
                 context.beginPath();
             }
 
+            // context.globalCompositeOperation = 'source-in';
+            // if (!this._cache.context) {
+            // } else {
+            //     // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/putImageData
+            //     context.putImageData(this._cache.image, moveX, moveY);
+            // }
+
             var moveX = ~~(0.5 + (this._segments[0].x * zoom + motion.x)),
                 moveY = ~~(0.5 + (this._segments[0].y * zoom + motion.y));
 
-            if (!this._cache.context) {
-                // movendo para o inicio do shape para não criar uma linha
-                context.moveTo(moveX, moveY);
-
-                // para cada segmento, vou traçando uma linha
-                for (var i = 0; i < this._segments.length; i++) {
-                    // http://seb.ly/2011/02/html5-canvas-sprite-optimisation/
-                    // http://jsperf.com/math-round-vs-hack/3
-                    // http://www.ibm.com/developerworks/library/wa-canvashtml5layering/
-                    var x = ~~(0.5 + (this._segments[i].x * zoom + motion.x));
-                    var y = ~~(0.5 + (this._segments[i].y * zoom + motion.y));
-
-                    context.lineTo(x, y);
-                }
-            } else {
-                // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/getImageData
-                var dt = this._cache.context.getImageData(1, 1, 100, 100);
-
-                // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/putImageData
-                context.putImageData(dt, 1, 1);
-                //context.putImageData(dt, moveX, moveY);
-            }
-            
-            // quando possivel personalização
-            if (this.style) {
-                // desenho o shape no contexto
-                context.stroke();
-                // restauro as configurações de estilo anteriores do contexto
-                context.restore();
-            }
-        },
-        render: function (context, transform) {
-
-            // possivel personalização
-            if (this.style) {
-                // salvo as configurações de estilo atuais do contexto
-                context.save();
-
-                // personalização para linha pontilhada
-                if (this.style.lineDash)
-                    context.setLineDash([5, 2]);
-
-                // personalização para a espessura da linha
-                if (this.style.lineWidth)
-                    context.lineWidth = this.style.lineWidth;
-
-                // personalização para a cor da linha
-                if (this.style.lineColor)
-                    context.strokeStyle = this.style.lineColor;
-
-                // e deixo iniciado um novo shape
-                context.beginPath();
-            }
-
-            // de acordo com a matrix - a escala que devo aplicar nos segmentos
-            var scale = Math.sqrt(transform.a * transform.d);
-            // de acordo com a matrix - o movimento que devo aplicar nos segmentos
-            var move = {
-                x: transform.tx,
-                y: transform.ty
-            };
-
             // movendo para o inicio do shape para não criar uma linha
-            context.moveTo(this._segments[0].x * scale + move.x, this._segments[0].y * scale + move.y);
+            context.moveTo(moveX, moveY);
+
             // para cada segmento, vou traçando uma linha
             for (var i = 0; i < this._segments.length; i++) {
-                var x = this._segments[i].x * scale + move.x;
-                var y = this._segments[i].y * scale + move.y;
+                // http://seb.ly/2011/02/html5-canvas-sprite-optimisation/
+                // http://jsperf.com/math-round-vs-hack/3
+                // http://www.ibm.com/developerworks/library/wa-canvashtml5layering/
+                var x = ~~(0.5 + (this._segments[i].x * zoom + motion.x));
+                var y = ~~(0.5 + (this._segments[i].y * zoom + motion.y));
 
                 context.lineTo(x, y);
-                
-//                if (i === this._segments.length - 2)
-//                    break;
-                
             }
-            
+
             // quando possivel personalização
             if (this.style) {
                 // desenho o shape no contexto
@@ -200,9 +166,7 @@
                 // restauro as configurações de estilo anteriores do contexto
                 context.restore();
             }
-
         }
-
     };
 
     var operation = {
