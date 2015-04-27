a2652a27663157b1498d7753c4f4b5d9e94c6e22
Ciro Cesar Maciel <me@ciro-maciel.me>
2015-04-27 16:57:40 -0300

    desenvolvendo group in group

--- src/core/group.js
+++ src/core/group.js
@@ -4,58 +4,6 @@
     var _groups = null, // store - para armazenamento 
         _shapes = null; // tree - para a arvore de pesquisa
 
-    function Group(attrs) {
-        this.uuid = attrs.uuid;
-        this.name = attrs.name;
-        this.status = attrs.status;
-        this.style = attrs.style;
-        this.children = attrs.children;
-        this.parent = attrs.parent;
-    }
-
-    Group.prototype._render = function (context, zoom, motion) {
-
-        var shapes = this.children.list();
-
-        // se não tenho estilo
-        if (!this.style) {
-            // inicio o conjunto de shapes no contexto
-            context.beginPath();
-        }
-
-        var i = 0;
-        do {
-            // se tenho estilo, passo a herança, caso contrario, limpo qualquer erro
-            shapes[i].style = this.style ? this.style : null;
-
-            shapes[i]._render(context, zoom, motion);
-            i++;
-        } while (i < shapes.length)
-
-        // se não tenho estilo
-        if (!this.style) {
-            // desenho o conjunto de shapes no contexto
-            context.stroke();
-        }
-
-        return true;
-    };
-
-    Group.prototype.toObject = function () {
-        return {
-            uuid: this.uuid,
-            name: this.name,
-            status: this.status, // para ativo || não ativo
-            style: this.style,
-            parent: this.parent.toObject(),
-            children: this.children.list().map(function (shape) {
-                return shape.toObject();
-            })
-        };
-    };
-
-
-
     plane.group = {
         _initialize: function (config) {
 
@@ -73,67 +21,32 @@
             return true;
 
         },
-        create: function (attrs, layerUuid) {
+        create: function (attrs) {
             if (plane.utility.conversion.toType(attrs) !== 'object') {
                 throw new Error('group - create - attrs is not valid \n http://plane.c37.co/docs/errors.html#' + 'errorCode');
             }
             if ((!attrs.children) || (plane.utility.conversion.toType(attrs.children) !== 'array')) {
                 throw new Error('group - create - children is not valid \n http://plane.c37.co/docs/errors.html#' + 'errorCode');
             }
-            if ((attrs.parent) && (plane.utility.conversion.toType(attrs.parent) !== 'object')) {
-                throw new Error('group - create - parent is not valid \n http://plane.c37.co/docs/errors.html#' + 'errorCode');
-            }
-
-            var group = null, // para o novo group
-                uuid = plane.utility.math.uuid(9, 16), // identificador do group
-                shapes = attrs.children; // os shapes filhos
 
             // a layer que vamos trabalhar
-            var layer = plane.layer.get(layerUuid);
+            var layer = plane.layer.active;
 
             // verifico se temos os stores para a layer que estamos trabalhando
             if ((!_groups.get(layer.uuid)) && (!_shapes.get(layer.uuid))) {
                 // se não existir, crio
-                _groups.add(layer.uuid, plane.math.dictionary.create());
-                _shapes.add(layer.uuid, plane.math.store.create());
+                //_groups.add(layer.uuid, plane.math.dictionary.create());
+                _groups.add(layer.uuid, plane.math.store.create());
+                //_shapes.add(layer.uuid, plane.math.store.create());
             }
 
-            // apagando a referencia em attrs
-            delete attrs.children;
-
-            // (attributos || parametros) para o novo Group
-            attrs = plane.utility.object.merge({
-                uuid: uuid,
-                name: 'Group '.concat(uuid),
-                parent: attrs.parent,
-                children: plane.math.dictionary.create()
-            }, attrs);
+//            debugger;
 
             // crio o novo Group
-            group = new Group(attrs);
+            var group = new plane.math.group.create(attrs);
 
-            // para conversão && add de children
-            var i = 0;
-            do {
-                if ((typeof shapes[i] !== 'object') || (!attrs.children)) {
-                    throw new Error('group - create - children is not valid \n http://plane.c37.co/docs/errors.html#' + 'errorCode');
-                }
-
-                // removo o shape de plane
-                plane.shape.remove(shapes[i].uuid, layer.uuid);
-
-                // para incluir dentro do group criado
-                group.children.add(shapes[i].uuid, shapes[i]);
-
-                // de acordo com a layer - add bounds in store
-                _shapes.get(layer.uuid).add(shapes[i].uuid, [shapes[i]._bounds.from.x, shapes[i]._bounds.from.y, shapes[i]._bounds.to.x, shapes[i]._bounds.to.y, shapes[i]]);
-
-                i++;
-            } while (i < shapes.length);
-            // para conversão && add de children
-
-            // de acordo com a layer - add shape in store
-            _groups.get(layer.uuid).add(group.uuid, group);
+            // de acordo com a layer - add bounds in store
+            _groups.get(layer.uuid).add(group.uuid, [group._bounds.from.x, group._bounds.from.y, group._bounds.to.x, group._bounds.to.y, group]);
 
             return group;
         },
@@ -142,21 +55,9 @@
                 throw new Error('group - remove - uuid is not valid \n http://plane.c37.co/docs/errors.html#' + 'errorCode');
             } else {
                 // a layer que vamos trabalhar
-                var layer = plane.layer.active,
-                    group = _groups.get(layer.uuid).get(uuid);
+                var layer = plane.layer.active;
 
-                group.children.list().forEach(function (shape) {
-                    // removo do store interdo de shapes
-                    _shapes.remove(shape.uuid);
-
-                    // nunca mantenho o estado de estilos
-                    delete shape.style;
-
-                    //crio em shapes sem group
-                    plane.shape.create(shape);
-                });
-
-                _groups.get(layer.uuid).remove(group.uuid);
+                _groups.get(layer.uuid).remove(uuid);
 
                 return true;
             }
@@ -201,21 +102,14 @@
                 var layer = plane.layer.get(uuid);
 
                 // verifico se criei ao menos um group para a layer
-                if (_shapes.get(layer.uuid)) {
+                if (_groups.get(layer.uuid)) {
 
-                    var shapes = null,
-                        rectangles = _shapes.get(layer.uuid).search(rectangle);
+                    // os rectangles selecionados
+                    var rectangles = _groups.get(layer.uuid).search(rectangle);
 
-                    // um mapeamendo para separar os uuids dos shapes
-                    shapes = rectangles.map(function (data) {
+                    // mapeamendo para separar os shapes dos rectangles selecionados
+                    return rectangles.map(function (data) {
                         return data[4];
-                    });
-
-                    // agora procuro e retorno só os grupos com os shapes encontrados
-                    return _groups.get(layer.uuid).list().filter(function (group) {
-                        return group.children.has(shapes.map(function (shape) {
-                            return shape.uuid;
-                        }));
                     });
 
                 } else {
---
+++ src/math/group.js
@@ -0,0 +1,188 @@
+(function (plane) {
+    "use strict";
+
+    function Group(attrs) {
+
+        this.uuid = null;
+        this.name = null;
+
+        this._segments = [];
+        this._bounds = null;
+
+        this.status = null;
+        this.style = null;
+
+        this.children = null;
+
+        this._initialize(attrs);
+    }
+
+    Group.prototype = {
+        _initialize: function (attrs) {
+
+            var uuid = plane.utility.math.uuid(9, 16), // identificador do group
+                children = attrs.children.slice(); // os objects filhos
+
+            delete attrs.children;
+
+            // (attributos || parametros) para o novo Group
+            attrs = plane.utility.object.merge({
+                uuid: uuid,
+                name: 'Group - '.concat(uuid),
+                children: plane.math.dictionary.create()
+            }, attrs);
+
+            //debugger;
+
+            // parse to group
+            var i = 0;
+            do {
+
+                if (children[i] instanceof Group) {
+                    plane.group.remove(children[i].uuid);
+                }
+
+                if (children[i] instanceof plane.math.shape) {
+
+                    this._segments = children[i]._segments.concat(this._segments);
+
+                    plane.shape.remove(children[i].uuid);
+                }
+
+                attrs.children.add(children[i].uuid, children[i]);
+
+                i++;
+            } while (i < children.length);
+
+
+
+
+            // completando os campos do group
+            plane.utility.object.extend(this, attrs);
+
+            //debugger;
+
+            // calculando os limites
+            this._calculeBounds();
+
+            return true;
+        },
+        _calculeBounds: function () {
+
+            var shapes = shapesOfChildren(this);
+
+            var from = plane.point.create(shapes[0]._segments[0]),
+                to = plane.point.create(shapes[0]._segments[0]);
+
+
+            shapes.forEach(function (shape) {
+                shape._segments.forEach(function (segment) {
+                    from = operation.minimum(segment, from);
+                    to = operation.maximum(segment, to);
+                });
+            });
+
+            this._bounds = plane.math.bounds.create(from, to);
+
+            return true;
+
+        },
+        contains: function (position, transform) {
+
+            return false;
+
+        },
+        intersect: function (rectangle) {
+
+            return true;
+
+        },
+        _render: function (context, zoom, motion) {
+
+            var children = this.children.list();
+
+            // sort, todo(s) o(s) group(s) devem ser as primeiras
+            // para organizarmos o context.beginPath()
+            children.sort(function (object) {
+                if (!(object instanceof  Group))
+                    return 1;
+                if ((object instanceof  Group))
+                    return -1;
+                return 0;
+            });
+
+            // se não tenho estilo
+            if (!this.style) {
+                // inicio o conjunto de shapes no contexto
+                context.beginPath();
+            }
+
+            var i = 0;
+            do {
+                // se tenho estilo, passo a herança, caso contrario, limpo qualquer estilo
+                children[i].style = this.style ? this.style : null;
+
+                children[i]._render(context, zoom, motion);
+
+                i++;
+            } while (i < children.length)
+
+            // se não tenho estilo
+            if (!this.style) {
+                // desenho o conjunto de shapes no contexto
+                context.stroke();
+            }
+
+            return true;
+
+        },
+        toObject: function () {
+            return {
+                uuid: this.uuid,
+                name: this.name,
+                status: this.status, // para ativo || não ativo
+                style: this.style,
+                children: this.children.list().map(function (shape) {
+                    return shape.toObject();
+                })
+            };
+        }
+    };
+
+    function shapesOfChildren(group) {
+
+        var shapes = [];
+
+        group.children.list().forEach(function (children) {
+            if (children instanceof  Group) {
+                shapes = shapes.concat(shapesOfChildren(children));
+            } else {
+                shapes.push(children);
+            }
+        });
+
+        return shapes;
+    }
+
+    var operation = {
+        minimum: function (a, b) {
+            return {
+                x: (a.x < b.x) ? a.x : b.x,
+                y: (a.y < b.y) ? a.y : b.y
+            };
+        },
+        maximum: function (a, b) {
+            return {
+                x: (a.x > b.x) ? a.x : b.x,
+                y: (a.y > b.y) ? a.y : b.y
+            };
+        }
+    };
+
+    plane.math.group = {
+        create: function (attrs) {
+            return new Group(attrs);
+        }
+    };
+
+})(c37.library.plane);
